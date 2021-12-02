8b05aaf8e9ead6e122b160fb7292124823312c63
mata0050 <mata0050@outlook.com>
2021-12-01 19:48:12 -0500

    finished

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,32 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
+const wordSearch = (letters, word) => {
+  const horizontalJoin = letters.map((ls) => ls.join(''));
+  const transposeMatrix = [];
+  const rowLength = letters[0].length;
+  let vertJoin;
+  console.log(horizontalJoin);
 
-module.exports = wordSearch
+  for (l of horizontalJoin) {
+    if (l.includes(word)) {
+      return true;
+    } else {
+      for (let i = 0; i < rowLength; i++) {
+        const row = [];
+        for (let arr of letters) {
+          const value = arr[i];
+          row.push(value);
+        }
+        transposeMatrix.push(row);
+      }
+      vertJoin = transposeMatrix.map((ls) => ls.join(''));
+      console.log(vertJoin);
+    }
+  }
+  for (k of vertJoin) {
+    if (k.includes(word)) {
+      return true;
+    }
+  }
+  return false;
+};
+
+module.exports = wordSearch;
